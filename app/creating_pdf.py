dc26061225a8305c7ce47b84c07a564952a0f57a
dorota10 <dorota.baczkiewicz@gmail.com>
2024-04-06 11:10:39 +0200

    umowa_pojazd

--- app/creating_pdf.py
+++ app/creating_pdf.py
@@ -36,29 +36,84 @@ def create_pdf_najem(data):
     return response
 
 
-def create_pdf_pojazd(data):
+# def create_pdf_pojazd(data):
 
+#     response = make_response()
+#     response.headers['Content-Type'] = 'application/pdf'
+#     response.headers['Content-Disposition'] = 'attachment; filename=umowa_sprzedazypojazdu.pdf'
+
+
+#     c = canvas.Canvas(response.stream, pagesize=letter)
+
+#     pdfmetrics.registerFont(TTFont('Verdana', 'Verdana.ttf'))
+#     c.setFont("Verdana", 10)
+
+#     text = c.beginText(40, 750)
+#     text.textLine("Umowa Sprzedaży Pojazdu")
+#     text.textLine(f"Umowa zawarta dnia {data['data_zawarcia'].strftime('%d-%m-%Y')} w miejscowości {data['miejsce_zawarcia']} pomiędzy:")
+#     text.textLine(f"{data['imie_sprzedajacego']} {data['nazwisko_sprzedajacego']}")
+#     text.textLine(f"zamieszkałą/łym w {data['miasto_sprzedajacego']} przy {data['ulica_sprzedajacego']} {data['numer_sprzedajacego']},")
+#     text.textLine(f"legitymującym się dowodem osobistym {data['dowod_sprzedajacego']} wydanym przez {data['wydawca_sprzedajacego']},")
+#     text.textLine(f"nr PESEL: {data['pesel_sprzedajacego']}, nazywana/y dalej Sprzedającą/cym")
+#     text.textLine("a")
+#     text.textLine(f"{data['imie_kupujacego']} {data['nazwisko_kupujacego']}")
+#     text.textLine(f"zamieszkałą/łym w {data['miasto_kupujacego']} przy {data['ulica_kupujacego']} {data['numer_kupujacego']},")
+#     text.textLine(f"legitymującym się dowodem osobistym {data['dowod_kupujacego']} wydanym przez {data['wydawca_kupujacego']},")
+#     text.textLine(f"nr PESEL: {data['pesel_kupujacego']}, nazywana/y dalej Kupującą/cym")  
+#     text.textLine(f"Przedmiotem umowy jest sprzedaż pojazdu marki {data['marka_auta']}, model {data['model_auta']}, rok produkcji {data['rok_produkcji']}, numer nadwozia {data['numer_vin']}, numer rejestracyjny {data['numer_rejestracyjny']}.")
+
+#     text.textLine(f"Sprzedający oświadcza, że nabył auto ze stanem drogomierza {data['przebieg_start']} km i od tego czasu pokonał nim {data['przebieg_pokonany']} km.")
+#     text.textLine("Sprzedający oświadcza, że pojazd określony w §1 stanowi jego własność, jest wolny od wad fizycznych i prawnych oraz nie stanowi przedmiotu zabezpieczenia.")
+    
+#     text.textLine(f"Strony ustaliły wartość przedmiotu umowy na kwotę: {data['cena']}")
+
+#     text.textLine(f"")
+#     c.drawText(text)
+#     c.showPage()
+#     c.save()
+
+#     return response
+
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import textwrap
+
+def create_pdf_pojazd(data):
     response = make_response()
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'attachment; filename=umowa_sprzedazypojazdu.pdf'
 
-
     c = canvas.Canvas(response.stream, pagesize=letter)
-
     pdfmetrics.registerFont(TTFont('Verdana', 'Verdana.ttf'))
     c.setFont("Verdana", 10)
 
-    text = c.beginText(40, 750)
-    text.textLine(f"{data['miejsce_zawarcia']}, {data['data_zawarcia'].strftime('%d-%m-%Y')}")
-    text.textLine("Umowa Sprzedaży Pojazdu")
-    text.textLine("Umowa zostaje zawarta między:")
-    text.textLine(f"{data['imie_sprzedajacego']} {data['nazwisko_sprzedajacego']}, PESEL: {data['pesel_sprzedajacego']}, zwanym dalej Sprzedającym")
-    text.textLine("a")
-    text.textLine(f"{data['imie_kupujacego']} {data['nazwisko_kupujacego']}, PESEL: {data['pesel_kupujacego']}, zwanym dalej Kupującym.")
-    text.textLine(f"Umowa tyczy się pojazdu marki {data['marka_auta']}, model {data['model_auta']}, rok produkcji {data['rok_produkcji']}, o numerze VIM {data['numer_vim']}.")
-    text.textLine(f"W dniu zawarcia umowy przebieg auta wynosi {data['przebieg']}km. Cena wynosi {data['cena']} złotych. ")
+    text_lines = [
+        "Umowa Sprzedaży Pojazdu",
+        f"Umowa zawarta dnia {data['data_zawarcia'].strftime('%d-%m-%Y')} w miejscowości {data['miejsce_zawarcia']} pomiędzy:",
+        f"{data['imie_sprzedajacego']} {data['nazwisko_sprzedajacego']}",
+        f"zamieszkałą/łym w {data['miasto_sprzedajacego']} przy {data['ulica_sprzedajacego']} {data['numer_sprzedajacego']},",
+        f"legitymującym się dowodem osobistym {data['dowod_sprzedajacego']} wydanym przez {data['wydawca_sprzedajacego']},",
+        f"nr PESEL: {data['pesel_sprzedajacego']}, nazywana/y dalej Sprzedającą/cym",
+        "a",
+        f"{data['imie_kupujacego']} {data['nazwisko_kupujacego']}",
+        f"zamieszkałą/łym w {data['miasto_kupujacego']} przy {data['ulica_kupujacego']} {data['numer_kupujacego']},",
+        f"legitymującym się dowodem osobistym {data['dowod_kupujacego']} wydanym przez {data['wydawca_kupujacego']},",
+        f"nr PESEL: {data['pesel_kupujacego']}, nazywana/y dalej Kupującą/cym",
+        f"Przedmiotem umowy jest sprzedaż pojazdu marki {data['marka_auta']}, model {data['model_auta']}, rok produkcji {data['rok_produkcji']}, \nnumer nadwozia {data['numer_vin']}, numer rejestracyjny {data['numer_rejestracyjny']}.",
+        f"Sprzedający oświadcza, że nabył auto ze stanem drogomierza {data['przebieg_start']} km i od tego czasu pokonał nim {data['przebieg_pokonany']} km.",
+        "Sprzedający oświadcza, że pojazd określony w §1 stanowi jego własność, jest wolny od wad fizycznych i prawnych oraz nie stanowi przedmiotu zabezpieczenia.",
+        f"Strony ustaliły wartość przedmiotu umowy na kwotę: {data['cena']}"
+    ]
 
-    c.drawText(text)
+    # text = c.beginText(40, 750)
+    y = 750
+    for line in text_lines:
+        lines = textwrap.wrap(line, width=90)
+        for wrapped_line in lines:
+            c.drawString(40, y, wrapped_line)
+            y -= 20
+
+    # c.drawText(text)
     c.showPage()
     c.save()
 
--- app/routes.py
+++ app/routes.py
@@ -36,25 +36,45 @@ def formularz_najmu():
 
 @app.route('/auto', methods = ['GET', 'POST'])
 def formularz_auta():
-    form = UmowaAuto()
+    import json
+    with open("data.json", "r", encoding='utf-8') as f:
+        data_json = json.load(f)
+        from datetime import datetime
+
+        date_str = data_json["data_zawarcia"]
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+        data_json["data_zawarcia"] = date_obj
+
+    form = UmowaAuto(**data_json)
+    
     if form.validate_on_submit():
         data = {
-
             'miejsce_zawarcia': form.miejsce_zawarcia.data,
             'data_zawarcia': form.data_zawarcia.data,
             'imie_sprzedajacego': form.imie_sprzedajacego.data,
             'nazwisko_sprzedajacego': form.nazwisko_sprzedajacego.data,
+            'miasto_sprzedajacego': form.miasto_sprzedajacego.data,
+            'ulica_sprzedajacego': form.ulica_sprzedajacego.data,
+            'numer_sprzedajacego': form.numer_sprzedajacego.data,
             'pesel_sprzedajacego': form.pesel_sprzedajacego.data,
+            'dowod_sprzedajacego': form.dowod_sprzedajacego.data,
+            'wydawca_sprzedajacego': form.wydawca_sprzedajacego.data,
             'imie_kupujacego': form.imie_kupujacego.data,
             'nazwisko_kupujacego': form.nazwisko_kupujacego.data,
+            'miasto_kupujacego': form.miasto_kupujacego.data,
+            'ulica_kupujacego': form.ulica_kupujacego.data,
+            'numer_kupujacego': form.numer_kupujacego.data,
             'pesel_kupujacego': form.pesel_kupujacego.data,
+            'dowod_kupujacego': form.dowod_kupujacego.data,
+            'wydawca_kupujacego': form.wydawca_kupujacego.data,
             'marka_auta': form.marka_auta.data,
             'model_auta': form.model_auta.data,
             'rok_produkcji': form.rok_produkcji.data,
             'numer_rejestracyjny': form.numer_rejestracyjny.data,
-            'przebieg': form.przebieg.data,
-            'numer_vim': form.numer_vim.data,
+            'numer_vin': form.numer_vin.data,
             'cena': form.cena.data,
+            'przebieg_start': form.przebieg_start.data,
+            'przebieg_pokonany': form.przebieg_pokonany.data,
 
         }
         return create_pdf_pojazd(data)
